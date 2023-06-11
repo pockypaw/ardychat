4e6ed07d1330a99344a80c56ea5df17ef1dae74c
Muhamad Ridho Ardian <49930863+pockypaw@users.noreply.github.com>
2023-06-11 11:25:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,36 @@
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const { parse } = require('url');
-const next = require('next');
+const { Server } = require("socket.io");
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    const io = new Server();
+    const users = {};
 
-const server = createServer((req, res) => {
-  const parsedUrl = parse(req.url, true);
-  handle(req, res, parsedUrl);
-});
+    io.on("connection", (socket) => {
+      socket.on("changeNickname", (newNickname) => {
+        const oldNickname = users[socket.id];
+        users[socket.id] = newNickname;
+        io.emit("nicknameChanged", { oldNickname, newNickname });
+        io.emit("updateOnlineUsers", Object.values(users));
+      });
 
-const io = new Server(server, {
-  /* Socket.IO options */
-});
+      socket.on("chatMessage", (message) => {
+        const nickname = users[socket.id] || "Anonymous";
+        io.emit("chatMessage", { nickname, message });
+      });
 
-// Store nicknames and online users
-const users = {};
+      users[socket.id] = "Anonymous";
+      io.emit("updateOnlineUsers", Object.values(users));
 
-io.on('connection', (socket) => {
-  // Change nickname
-  socket.on('changeNickname', (newNickname) => {
-    const oldNickname = users[socket.id];
-    users[socket.id] = newNickname;
-    io.emit('nicknameChanged', { oldNickname, newNickname });
-    io.emit('updateOnlineUsers', Object.values(users));
-  });
+      socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("updateOnlineUsers", Object.values(users));
+      });
+    });
 
-  // Handle chat messages
-  socket.on('chatMessage', (message) => {
-    const nickname = users[socket.id] || 'Anonymous';
-    io.emit('chatMessage', { nickname, message });
-  });
-
-  // Add user to online users and update all clients
-  users[socket.id] = 'Anonymous';
-  io.emit('updateOnlineUsers', Object.values(users));
-
-  // Remove user from online users on disconnect
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('updateOnlineUsers', Object.values(users));
-  });
-});
-
-app.prepare().then(() => {
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
-});
+    // Start the serverless WebSocket
+    io.serveHttp(req, res);
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+};
