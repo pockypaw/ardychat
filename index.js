8147ff908485cf52b2d64947d77bcd34c69a4141
Muhamad Ridho Ardian <49930863+pockypaw@users.noreply.github.com>
2023-06-11 11:32:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send('<h1>Hello world</h1>');
 });
 
-// Store nicknames and online users
-const users = {};
-
-io.on('connection', (socket) => {
-  // Change nickname
-  socket.on('changeNickname', (newNickname) => {
-    const oldNickname = users[socket.id];
-    users[socket.id] = newNickname;
-    io.emit('nicknameChanged', { oldNickname, newNickname });
-    io.emit('updateOnlineUsers', Object.values(users));
-  });
-
-  // Handle chat messages
-  socket.on('chatMessage', (message) => {
-    const nickname = users[socket.id] || 'Anonymous';
-    io.emit('chatMessage', { nickname, message });
-  });
-
-  // Add user to online users and update all clients
-  users[socket.id] = 'Anonymous';
-  io.emit('updateOnlineUsers', Object.values(users));
-
-  // Remove user from online users on disconnect
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('updateOnlineUsers', Object.values(users));
-  });
-});
-
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
